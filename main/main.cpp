bbcb4febb21c248160595b50da429f3ea8352ec8
Oyalmli <oyvind.almli@gmail.com>
2022-05-23 12:34:09 +0200

    Name change

--- main/main.cpp
+++ main/main.cpp
@@ -1,34 +1,101 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "driver/adc.h"
-#include "dvfw/dvfw.hpp"
+#include "dfl/dfl.hpp"
 
 using namespace dvfw;
 
-class sensor_gen : public gen::base_generator<int> {
-    private:
-     bool* _flag;
-   public:
-    bool hasNext() {
-        return *_flag;
-    }
+template <typename Gen, typename T>
+class GenIt : public std::iterator<std::input_iterator_tag, T, bool, T*, T&> {
+ public:
+  Gen* _gen;
 
-    int next() {
-        sensor_gen::wait(10);
-        return hall_sensor_read();
-    }
+  GenIt(Gen* gen) : _gen{gen} {};
+  GenIt& operator++() {
+    _gen->next();
+    return *this;
+  }
 
-    sensor_gen(bool* flag): _flag{flag} {}
+  bool operator!=(GenIt& other) { return _gen->hasNext(); }
+
+  bool operator==(GenIt& other) { return !(_gen->hasNext()); }
+
+  T operator*() { return _gen->curr(); }
+};
+
+template <typename T>
+class bg {
+  using Iterator = GenIt<bg<T>, T>;
+
+ public:
+ using value_type = T;
+  virtual bool hasNext() = 0;
+  virtual T next() = 0;
+  virtual T curr() = 0;
+  virtual Iterator begin() { return Iterator(this); }
+  virtual Iterator end() { return Iterator(nullptr); }
+};
+
+template <typename T>
+class range : public bg<T> {
+ private:
+  T _min, _max, _step;
+
+ public:
+  T _itVal;
+  range() = default;
+  range(T max) : _min{0}, _max{max}, _step{1}, _itVal{0} {};
+  range(T min, T max) : _min{min}, _max{max}, _step{1}, _itVal{min} {};
+  range(T min, T max, T step)
+      : _min{min}, _max{max}, _step{step}, _itVal{min} {};
+
+  bool hasNext() { return _max > _itVal; }
+
+  T next() {
+    _itVal += _step;
+    return _itVal;
+  }
+
+  T curr() { return _itVal; }
+};
+
+template <class Gen>
+class take : public bg<typename Gen::value_type> {
+ private:
+  Gen _gen;
+  size_t _num;
+  size_t _taken{0};
+
+ public:
+  using value_type = typename Gen::value_type;
+  using T = value_type;
+
+  value_type _itVal = _gen._itVal;
+
+  take(size_t num, Gen generator) : _gen{generator}, _num{num} {}
+
+  bool hasNext() { return _gen.hasNext() && (_taken < _num); }
+
+  T next() {
+    _taken++;
+    _gen.next();
+    _itVal = _gen._itVal;
+    return _gen._itVal;
+  }
+
+  T curr(){
+    return _itVal;
+  }
+
+
 };
 
 extern "C" void app_main(void) {
-    bool flag = false;
-    auto sensor_generator = sensor_gen(&flag);
+    auto range_gen = take(10, range(100));
     auto pipeline
     =   pipe::moving_avg<int, 10>()
     >>= pipe::transform([](float i){ return static_cast<int>(i); })
-    >>= pipe::set_state([](bool& flag){ flag = false; }, flag)
     >>= sink::printf("%d\n"); 
     
-    sensor_generator >>= pipeline;
+    range_gen >>= pipeline;
 }
